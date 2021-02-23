067ce473031f0ab0feec9537d21c3c5c3810c4bc
Daniel Ledwos <daniel.ledwos@unilever.com>
2021-02-23 14:21:29 +0000

    general clean up

--- bin/index.js
+++ bin/index.js
@@ -6,14 +6,14 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-// Class for making bank account
+// TO DO
+// - Input validation -> everywhere
+// - Routing back to UIMain
+// - Clear screen? Stretch goal
 
-// variables - name, account number, money
-// fxn withdraw - changes money dependent on input
-// fxn deposit - changes money dependent on input
-// fxn close account - does what it says
 
-class bankAccount {
+// bank account Class
+class BankAccount {
     constructor(name, money)  {
         this.name = name;
         this.money = money;
@@ -40,10 +40,12 @@ class bankAccount {
 
 };
 
+// Terminal functions
+
 function UINewAcc() {
     rl.question("What is your name? ", function(resName) {
         rl.question("How much would you like to deposit? ", function(resMoney){
-            let user = new bankAccount(resName, resMoney);
+            let user = new BankAccount(resName, resMoney);
             console.log(`Name: ${user.name}, Money: ${user.money}`);
             // call main function
         });
@@ -106,29 +108,3 @@ function UIMain(userInfo) {
 };
 
 UIMain(null);
-
-
-
-
-
-// Shop simulator:
-// add a minigame to make purchase, thus showing transaction in action (or failing if account closed)
-
-
-
-function userInput() {
-    rl.question("how\'s it going? ", function(response) {
-        if (response === "good") {
-            console.log("That\'s great to hear! You're doing great :)");
-        } else if (response === "bad") {
-            console.log("Oh no! I\'m sure you can get over whatever is troubling you :)");
-        } else {
-            console.log("I\'m not sure I understood that. Hope you\'re doing good either way! \n \n Stay Positive :D");
-            rl.close();
-        }
-    });
-};
-
-// userInput();
-
-// UINewAcc();
