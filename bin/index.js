a24c63064735727eab8b9d93cf1d8c83ac4e62c3
Daniel Ledwos <daniel.ledwos@unilever.com>
2021-02-25 15:53:14 +0000

    add UI responses / better UI traversal

--- bin/index.js
+++ bin/index.js
@@ -8,7 +8,6 @@ const rl = readline.createInterface({
 
 // TO DO
 // - Input validation -> everywhere
-// - Routing back to UIMain
 // - Implement rl.close();
 // - Clear screen? Stretch goal
 
@@ -22,12 +21,17 @@ class BankAccount {
     };
 
     withdraw(amount) {
-        this.money = this.money - amount;
-        console.log(`new sum: ${this.money}`);
+        console.log(`Balance: £ ${this.money}`);
+        if (amount <= this.money) {
+            this.money = this.money - amount;
+            console.log(`new sum: ${this.money}`);
+        } else {
+            console.log("You don't have enough money in your account");
+        }
     };
 
     deposit(amount) {
-        this.money = this.money + amount;
+        this.money = Number(this.money) + Number(amount);
         console.log(`new sum: ${this.money}`);
     };
 
@@ -46,9 +50,10 @@ class BankAccount {
 function UINewAcc() {
     rl.question("What is your name? ", function(resName) {
         rl.question("How much would you like to deposit? ", function(resMoney){
-            let user = new BankAccount(resName, resMoney);
-            console.log(`Name: ${user.name}, Money: ${user.money}`);
+            let acc = new BankAccount(resName, resMoney);
+            console.log(`Name: ${acc.name}, Money: ${acc.money} \n\n`);
             // call main function
+            UIMain(acc);
         });
     });
 };
@@ -57,6 +62,7 @@ function UIWithdraw(account) {
     rl.question("How much money would you like to withdraw? ", function(reqMoney) {
         account.withdraw(reqMoney);
         // call main function
+        UIResponse(4, account);
     });
 };
 
@@ -64,6 +70,7 @@ function UIDeposit(account) {
     rl.question("How much money would you like to deposit? ", function(reqMoney) {
         account.deposit(reqMoney);
         // call main function
+        UIResponse(3, account);
     });
 };
 
@@ -71,39 +78,104 @@ function UICloseAcc(account) {
     rl.question(`You are about to close ${account.name}'s account, do you with to continue? (y/n)`, function(resClose) {
         if (resClose === "y") {
             account.closeAccount();
+        } else if (resClose === "n") {
+            UIMain();
         }
-        // call main function
     });
 }
 
 function UIShop(account) {
-    rl.question("Hello! What would you like to purchase? \n\n1- Pepperoni \n2- Hawaiian \n3- Margarita \n4- Chicken Supreme \n\n   Choice: ", function(item) {
+    rl.question("Hello! What would you like to purchase? \n\n1- Pepperoni : £12 \n2- Hawaiian : £14 \n3- Margarita : £8 \n4- Chicken Supreme : £12 \n5- Home \n\n   Choice: ", function(item) {
         if (item === "1") {
             console.log("Pepperoni, the classic!");
+            account.withdraw(12);
+            UIResponse(2, account);
         } else if (item === "2") {
             console.log("Hawaiian? sure, not judging");
+            account.withdraw(14);
+            UIResponse(2, account);
         } else if (item === "3") {
             console.log("Margarita, nice and simple");
-            rl.write(null, {ctrl: true, name: 'u'});
+            account.withdraw(8);
+            UIResponse(2, account);
         } else if (item === "4"){
             console.log("Chicken Supreme, delicious!");
+            account.withdraw(12);
+            UIResponse(2, account);
+        } else if (item === "5"){
+            UIMain(account);
         }
     });
 };
 
+// Generic responses for traversing in terminal
+function UIResponse(res, accRes) {
+    switch (res) {
+        case 1:
+            rl.question("Open account? (y/n)", function(response) {
+                if (response === "y") {
+                    UINewAcc();
+                } else if (response === "n") {
+                    UIMain(accRes);
+                }
+            });
+            break;
+        case 2:
+            rl.question("Back to home? (y/n)", function(response) {
+                if (response === "y") {
+                    UIMain(accRes)
+                } else if (response === "n") {
+                    UIShop(accRes);
+                }
+            });
+            break;
+        case 3:
+            rl.question("Back to home? (y/n)", function(response) {
+                if (response === "y") {
+                    UIMain(accRes)
+                } else if (response === "n") {
+                    UIDeposit(accRes);
+                }
+            });
+            break;
+        case 4:
+            rl.question("Back to home? (y/n)", function(response) {
+                if (response === "y") {
+                    UIMain(accRes)
+                } else if (response === "n") {
+                    UIWithdraw(accRes);
+                }
+            });
+            break;
+        case 5:
+            rl.question("Back to home? (y/n)", function(response) {
+                if (response === "y") {
+                    UIMain()
+                } else if (response === "n") {
+                    UIResponse(5);
+                }
+            });
+            break;
+            
+    }
+};
+
 function UIMain(userInfo) {
     let user = userInfo;
-    rl.question("Welcome to JS Bank! \n \nwhat would you like to do today? \n(select a number to continue) \n\n1- Open Account \n2- Withdraw \n3- Deposit \n4- Go Shopping \n\n   Choice: ", function(input) {
+    rl.question("Welcome to JS Bank! \n \nwhat would you like to do today? \n(select a number to continue) \n\n1- Open Account \n2- Withdraw \n3- Deposit \n4- Check Balance \n5- Go Shopping \n\n   Choice: ", function(input) {
         if (input === "1") {
             console.log("Let\'s open an account!");
+            UINewAcc();
         } else if (input === "2") {
-            user ? console.log("Let\'s withdraw some moneeyyyy") : console.log("You don't have an account yet");
+            user ? (console.log("Let\'s withdraw some moneeyyyy \n"), UIWithdraw(user)) : (console.log("You don't have an account yet"), UIResponse(1));
         } else if (input === "3") {
-            console.log("Let\'s stock up on cash $$$");
-            rl.write(null, {ctrl: true, name: 'u'});
+            user ? (console.log("Let\'s stock up on cash $$$ \n"), UIDeposit(user)) : (console.log("You don't have an account yet"), UIResponse(1));
         } else if (input === "4"){
-            console.log("yass queen, let\'s go shopping!");
-            UIShop(user);
+            user ? (console.log(`Balance: £ ${user.money}`), UIResponse(5)) : (console.log("You want money? Open an account first!"), UIResponse(1));
+        } else if (input === "5"){
+            user ? (console.log("let\'s go shopping!"), UIShop(user)) : (console.log("You're trying to go shopping without an account? \nEverything is digital now, open an account :)\n\n"), UIResponse(1));
+        } else if (input === "6") {
+            user ? UICloseAcc(user) : UIResponse(1);
         }
     });
 };
